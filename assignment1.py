9a549df55acc980ba718958b86202ac428de9e33
waqasfeng <engr.waqaskhan786@gmail.com>
2024-07-18 15:51:32 +0500

    first commit

---
+++ assignment1.py
@@ -0,0 +1,6 @@
+cities = ["peshawar", "islamabad", "lahore", "quetta", "karachi"]
+user_input = input ("please enter a city name: ")
+if user_input in cities:
+    print ("this city is one of the cleanest.")
+else:
+    print ("this city might be one of the dirtiest.")
