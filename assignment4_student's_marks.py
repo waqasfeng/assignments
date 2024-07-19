e60cdac897daecaa746c7ec80b331255bd88524b
waqasfeng <engr.waqaskhan786@gmail.com>
2024-07-19 16:10:49 +0500

    assignement 2

--- assignment4_student's_marks.py
+++ assignment4_student's_marks.py
@@ -1,4 +1,4 @@
  # Assignment 4.
 x = int(input("please enter the student's marks: "))
-Grade = "A Grade" if x >= 90 else ("B Grade" if x >= 80 else "fail")
+Grade = "A Grade" if x >= 90 else ("B Grade" if x >= 80 else "failed")
 print (Grade)
